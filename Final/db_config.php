8e3bd7b5fbd47cf511230a9ac7ee225c7207430a
Parima15-creator <parimatendulkar@gmail.com>
2026-06-27 14:37:58 +0530

    Prepare DeadlineRX for Google Cloud deployment

--- Final/db_config.php
+++ Final/db_config.php
@@ -1,12 +1,21 @@
 <?php
-$host = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_name = "deadlinerx";
+$db_user = getenv("DB_USER") ?: "root";
+$db_pass = getenv("DB_PASS") ?: "";
+$db_name = getenv("DB_NAME") ?: "deadlinerx";
 
-$conn = new mysqli($host, $db_user, $db_pass, $db_name);
+$cloud_sql_socket = getenv("CLOUD_SQL_SOCKET");
+
+if ($cloud_sql_socket) {
+    $conn = new mysqli(null, $db_user, $db_pass, $db_name, null, $cloud_sql_socket);
+} else {
+    $host = getenv("DB_HOST") ?: "localhost";
+    $conn = new mysqli($host, $db_user, $db_pass, $db_name);
+}
 
 if ($conn->connect_error) {
-    die(json_encode(['success' => false, 'message' => "Database Connection failed"]));
+    die(json_encode([
+        'success' => false,
+        'message' => "Database Connection failed"
+    ]));
 }
 ?>
